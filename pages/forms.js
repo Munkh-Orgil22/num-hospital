8662fb53c31c0617e93fb7ceb3e7306aa11b2365
amarjargalamraa61 <amarjargalamraa61@gmail.com>
2022-04-20 13:46:27 +0800

    8

--- pages/forms.js
+++ pages/forms.js
@@ -1,52 +1,91 @@
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import {
   Grid,
   Stack,
   TextField,
-  Checkbox,
-  FormGroup,
-  FormControlLabel,
-  RadioGroup,
-  Radio,
-  FormLabel,
-  FormControl,
+  InputLabel,
+  Dialog,
+  MenuItem,
   Button,
+  Alert,
 } from "@mui/material";
+
+
+import React, { useState, useEffect } from "react";
 import BaseCard from "../src/components/baseCard/BaseCard";
 
 const Forms = () => {
+  const [allergies, setallergies] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setallergies(e.target.value);
+  };
+
+  const handleOnClick = () => {
+    setOpen(!open);
+  }
+
+
   return (
     <Grid container spacing={0}>
       <Grid item xs={12} lg={12}>
-        <BaseCard title="Form Layout">
+        <BaseCard title="Хэрэглэгч бүртгэх хэсэг">
           <Stack spacing={3}>
             <TextField
               id="name-basic"
-              label="Name"
+              label="Нэр"
               variant="outlined"
-              defaultValue="Nirav Joshi"
             />
-            <TextField id="email-basic" label="Email" variant="outlined" />
             <TextField
-              id="pass-basic"
-              label="Password"
-              type="password"
+              id="name-basic"
+              label="Овог"
               variant="outlined"
             />
+
+            <TextField id="email-basic" label="регистрийн дугаар" variant="outlined" />
+            <InputLabel id="demo-simple-select-label">Харшилтай юу?</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={allergies}
+              label="Харшил"
+              variant="outlined"
+              onChange={handleChange}
+            >
+              <MenuItem value={1}>Тийм</MenuItem>
+              <MenuItem value={0}>Үгүй</MenuItem>
+            </Select>
             <TextField
               id="outlined-multiline-static"
-              label="Text Area"
+              label="Гэрийн хаяг"
               multiline
               rows={4}
-              defaultValue="Default Value"
             />
-            <TextField
+            {/* <TextField
               error
               id="er-basic"
               label="Error"
               defaultValue="ad1avi"
               variant="outlined"
+            /> */}
+            <TextField
+              id="name-basic"
+              label="Боловсрол"
+              variant="outlined"
             />
-            <FormGroup>
+            <TextField
+              id="name-basic"
+              label="Мэрэгжил"
+              variant="outlined"
+            />
+            <TextField
+              id="name-basic"
+              label="Ажилийн газарийн хаяг"
+              variant="outlined"
+            />
+            {/* <FormGroup>
               <FormControlLabel
                 control={<Checkbox defaultChecked />}
                 label="Terms & Condition"
@@ -56,8 +95,8 @@ const Forms = () => {
                 control={<Checkbox />}
                 label="Disabled"
               />
-            </FormGroup>
-            <FormControl>
+            </FormGroup> */}
+            {/* <FormControl>
               <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
               <RadioGroup
                 aria-labelledby="demo-radio-buttons-group-label"
@@ -80,16 +119,21 @@ const Forms = () => {
                   label="Other"
                 />
               </RadioGroup>
-            </FormControl>
+            </FormControl> */}
           </Stack>
           <br />
-          <Button variant="contained" mt={2}>
+          <Button variant="contained" mt={2} onClick={handleOnClick}  >
             Submit
           </Button>
+          <Dialog open={open} onClose={handleOnClick}>
+          <Alert  severity="success">
+              Амжилтай хадгаллаа
+            </Alert>
+          </Dialog>
         </BaseCard>
       </Grid>
 
-      <Grid item xs={12} lg={12}>
+      {/* <Grid item xs={12} lg={12}>
         <BaseCard title="Form Design Type">
           <Stack spacing={3} direction="row">
             <TextField
@@ -105,7 +149,7 @@ const Forms = () => {
             />
           </Stack>
         </BaseCard>
-      </Grid>
+      </Grid> */}
     </Grid>
   );
 };
